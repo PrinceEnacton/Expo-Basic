646b375818b563c2767873414ecfdc842f52d9e6
PrinceEnacton <pdhirapara.enacton@gmail.com>
2024-02-21 16:23:40 +0530

    Fix: now displaying activity Indicator

--- src/Screen/Home/index.js
+++ src/Screen/Home/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Grid from "../../Component/Core/Grid";
 import CustomButton from "../../Component/Core/Button";
+import CustomActivityIndicator from "../../Component/Core/ActivityIndicator";
 
 function HomeScreen(props) {
   // return <Form />;
   // return <Grid />;
-  return <CustomButton />;
+  // return <CustomButton />;
+  return <CustomActivityIndicator />;
 }
 export default HomeScreen;
 
