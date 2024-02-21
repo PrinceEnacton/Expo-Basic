99fda3e7ac371d514da3d7cb6e1e449650af2ac7
PrinceEnacton <pdhirapara.enacton@gmail.com>
2024-02-21 14:45:36 +0530

    Fix : color config added

--- src/Asset/Theme/colors.js
+++ src/Asset/Theme/colors.js
@@ -1,4 +1,11 @@
-export default {
+// export default {
+//   primary: "black",
+//   secondary: "#6828AF",
+//   white: "#fff",
+//   yellow: "yellow",
+// };
+
+export const ColorConfig = {
   primary: "black",
   secondary: "#6828AF",
   white: "#fff",
